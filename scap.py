9ba448701af8f26ba92d833b4d9be4950b002858
alfonsotejeda <alfonsotejeda150605@gmail.com>
2024-05-25 17:28:15 -0400

    new_env

--- scap.py
+++ scap.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
+def save_lineup():
+    with open('lineups.tex' , 'a') as f:
+        f.write(f'{link} \n')
+        f.write("-------------------------------------------------------------")
+        for player in player_names:
+            f.write(player)
+
 # URL of the page to scrape
 url = 'https://www.besoccer.com/competition/teams/summer_olympics'
 
@@ -22,20 +29,22 @@ for li in li_items:
         teams[team_name] = { "url": a_tag['href']}
 # Get all href
 team_link = [team['url'] for team in teams.values()]
-for link in team_link:
-    response = requests.get(link)
-    soup = BeautifulSoup(response.content , 'html.parser')
-    menu_links = soup.find_all('a', class_="menu-item")
-    for option in menu_links:
-        if "Squad" in option:
-            squad_link = option['href']
-    response = requests.get(squad_link)
-    soup = BeautifulSoup(response.content , 'html.parser')
-    player_names = [td.get_text() for td in soup.find_all('td', class_='name')]
-    with open('lineups.tex' , 'a') as f:
-        f.write(f'{link} \n')
-        f.write("-------------------------------------------------------------")
-        for player in player_names:
-            f.write(player)
+link = team_link[0]
+response = requests.get(link)
+soup = BeautifulSoup(response.content , 'html.parser')
+menu_links = soup.find_all('a', class_="menu-item")
+for option in menu_links:
+    if "Squad" in option:
+        squad_link = option['href']
+response = requests.get(squad_link)
+soup = BeautifulSoup(response.content , 'html.parser')
+competicion = soup.find('select' , id = "competition")
+competicion_value = [option['value'] for option in competicion.find_all('option')]
+url = competicion_value[0]
+response = requests.get(url)
+soup = BeautifulSoup(response.content , 'html.parser')
+print(soup)
+
+player_names = [td.get_text() for td in soup.find_all('td', class_='name')]
 
 # Hace falta scrapear por las competiciones y quedarse con el squad mas reciente guardado
